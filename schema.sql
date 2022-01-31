235061244b406e60325fe91fe1cd04dee0d98ad9
Matias Nisula <matiasnisula81@gmail.com>
2022-01-31 16:32:36 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
