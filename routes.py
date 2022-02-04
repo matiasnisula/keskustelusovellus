406a7e52a358ca8f1daabb789186a630d5738d1a
Matias Nisula <matiasnisula81@gmail.com>
2022-02-04 14:43:49 +0200

    functionality login, register, logout

--- routes.py
+++ routes.py
@@ -1,7 +1,41 @@
+from crypt import methods
 from app import app
-import users
-from flask import render_template
+from flask import render_template, request, redirect
+import messages, users, subjects, threads
 
 @app.route("/")
 def index():
-    return "sd"
+    list = subjects.get_list()
+    return render_template("index.html", subjects=list)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
