fda7415a5dda5dff98f4d0c4e725dfcfb83bd4e8
Matias Nisula <matiasnisula81@gmail.com>
2022-02-04 13:22:06 +0200

    new schema.sql

--- schema.sql
+++ schema.sql
@@ -2,12 +2,25 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
     password TEXT,
-    admin BOOLEAN
+    admin INTEGER
+);
+
+CREATE TABLE subjects (
+    id SERIAL PRIMARY KEY,
+    title TEXT UNIQUE
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    subject_id INTEGER REFERENCES subjects,
+    user_id INTEGER REFERENCES users
 );
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     user_id INTEGER REFERENCES users,
+    thread_id INTEGER REFERENCES threads,
     sent_at TIMESTAMP
 );
