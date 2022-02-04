86c3a1da0995fae09811fc67f79b9e74115f4532
Matias Nisula <matiasnisula81@gmail.com>
2022-02-04 14:54:25 +0200

    requesthandler, add new subject, add new thread to subject, add new message to thread

--- routes.py
+++ routes.py
@@ -8,6 +8,35 @@ def index():
     list = subjects.get_list()
     return render_template("index.html", subjects=list)
 
+@app.route("/subject/<int:id>", methods=["GET"])
+def subject(id):
+    list = threads.get_list(id)
+    return render_template("subject.html", threads=list, id=id)
+
+
+@app.route("/subject", methods=["POST"])
+def send_subject():
+    subject = request.form["subject"]
+    if subjects.save(subject):
+        return redirect("/")
+    else:
+        return render_template("error.html", message="Aiheen lisääminen ei onnistunut")
+
+@app.route("/subject/<int:id>/newthread", methods=["GET","POST"])
+def send_thread(id):
+    if request.method == "GET":
+        return render_template("newthread.html", id=id)
+    if request.method == "POST":
+        title = request.form["title"]
+        thread_id = threads.save_new(title, id)
+        if thread_id != 0:
+            message_content = request.form["content"]
+            if messages.save_new(message_content, thread_id):
+                return redirect("/subject/"+str(id))
+        else:
+            return render_template("error.html", message="Viestiketjun lisääminen epäonnistui")
+    
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
