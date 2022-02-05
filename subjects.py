b3a6cd44c696b20160c25fd26e222b866b1bba38
Matias Nisula <matiasnisula81@gmail.com>
2022-02-05 16:34:25 +0200

    db functionality get subject title, fixed view subject.html

--- subjects.py
+++ subjects.py
@@ -2,16 +2,24 @@ from db import db
 import users
 
 
-def get_list():
-    sql = "SELECT * FROM subjects"
+def get_all():
+    sql = "SELECT id, title FROM subjects"
     result = db.session.execute(sql)
     return result.fetchall()
+
+def get_title(id):
+    sql = "SELECT title FROM subjects WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchone()
 
 def save(title:str):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO subjects (title) VALUES (:title)"
-    db.session.execute(sql, {"title":title})
-    db.session.commit()
+    try:
+        sql = "INSERT INTO subjects (title) VALUES (:title)"
+        db.session.execute(sql, {"title":title})
+        db.session.commit()
+    except:
+        return False
     return True
