d242e9375ddd8549fc4ddc69ed0108c9ee31f860
Matias Nisula <matiasnisula81@gmail.com>
2022-02-17 18:00:56 +0200

    db functionality delete message

--- messages.py
+++ messages.py
@@ -3,19 +3,28 @@ import users
 
 
 def get_list(thread_id):
-    sql = "SELECT content, sent_at FROM messages WHERE thread_id=:id"
+    sql = "SELECT id, content, sent_at, user_id FROM messages WHERE thread_id=:id " \
+        "AND visible=TRUE"
     result = db.session.execute(sql, {"id":thread_id})
     return result.fetchall()
 
 
-def save_new(content, thread_id):
+def save(content, thread_id):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO messages (content, user_id, thread_id, sent_at) " \
-        "VALUES (:content, :user_id, :thread_id, NOW())"
-    db.session.execute(sql, {"content":content, "user_id":user_id, "thread_id":thread_id})
+    sql = "INSERT INTO messages (content, user_id, thread_id, sent_at, visible) " \
+        "VALUES (:content, :user_id, :thread_id, NOW(), :visible)"
+    db.session.execute(sql, {"content":content, "user_id":user_id, 
+                        "thread_id":thread_id, "visible":True})
     db.session.commit()
     return True
 
-
+def delete(id):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    sql = "UPDATE messages SET visible=FALSE WHERE id=:id AND user_id=:user_id"
+    db.session.execute(sql, {"id":id, "user_id":user_id})
+    db.session.commit()
+    return True
