2538840d3f7e3e2e85006c29e6ef4dcb40ddafda
Matias Nisula <matiasnisula81@gmail.com>
2022-02-18 13:38:14 +0200

    check user input not empty for subjects, threads, messages, show errormessage in same html file

--- routes.py
+++ routes.py
@@ -3,40 +3,54 @@ from email import message
 from app import app
 from flask import render_template, request, redirect, url_for
 import messages, users, subjects, threads
+import re
 
 @app.route("/")
 def index():
     list = subjects.get_all()
     return render_template("index.html", subjects=list)
 
+
 @app.route("/subject/<int:id>", methods=["GET"])
 def subject(id):
     list = threads.get_threads_on(id)
     subject = subjects.get_title(id)[0]
-    return render_template("subject.html", threads=list, id=id, subject=subject)
+    return render_template("threads.html", threads=list, id=id, subject=subject)
 
 
 @app.route("/subject", methods=["POST"])
 def send_subject():
-    subject = request.form["subject"]
+    subject = request.form["subject"].strip()
+    if subject == "":
+        return render_template("index.html", error_message="Älä jätä kenttää tyhjäksi!")
+
     if subjects.save(subject):
         return redirect("/")
     else:
-        return render_template("error.html", message="Aiheen lisääminen ei onnistunut")
+        return render_template("index.html", error_message="Aiheen lisääminen ei onnistunut")
+
 
 @app.route("/subject/<int:subject_id>/<int:thread_id>", methods=["GET", "POST"])
 def send_message(subject_id,thread_id):
+    message_list = messages.get_list(thread_id)
+    title = threads.get_title(thread_id)[0]
     if request.method == "GET":
-        message_list = messages.get_list(thread_id)
-        title = threads.get_title(thread_id)[0]
         return render_template("messages.html", messages=message_list, title=title, 
                             subject_id=subject_id, thread_id=thread_id)
     if request.method == "POST":
-        content = request.form["content"]
+        content = request.form["content"].strip()
+        if content == "":
+            return render_template("messages.html", messages=message_list, title=title, 
+                            subject_id=subject_id, thread_id=thread_id, 
+                            error_message="Viestikenttä oli tyhjä!")
+
         if messages.save(content, thread_id):
-            return redirect("/subject/"+str(subject_id)+"/"+str(thread_id))
+            url = f"/subject/{subject_id}/{thread_id}"
+            return redirect(url)
         else:
-            return render_template("error.html", message="Viestin lähettäminen epäonnistui")
+            return render_template("messages.html", messages=message_list, title=title, 
+                            subject_id=subject_id, thread_id=thread_id, 
+                            error_message="Viestin lähettäminen epäonnistui!")
 
 
 @app.route("/subject/<int:id>/newthread", methods=["GET","POST"])
@@ -46,17 +60,22 @@ def send_thread(id):
     if request.method == "GET":
         return render_template("newthread.html", id=id)
     if request.method == "POST":
-        if threads.save(request.form["title"], id):
+        thread = request.form["title"].strip()
+        if thread == "":
+            return render_template("newthread.html", id=id, error_message="Virhe: viestikenttä oli tyhjä!")
+        if threads.save(thread, id):
             return redirect("/subject/"+str(id))
         else:
-            return render_template("error.html", message="Viestiketjun lisääminen epäonnistui")
+            return render_template("newthread.html", id=id, error_message="Viestiketjun lisääminen epäonnistui")
+
 
 @app.route("/delete/<int:subject_id>/<int:id>", methods=["GET"])
 def delete_thread(subject_id, id):
     if threads.delete(id):
         return redirect("/subject/"+str(subject_id))
     else:
-        return render_template("error.html", message="Viestiketjun poistaminen epäonnistui")
+        return render_template("threads.html", error_message="Viestiketjun poistaminen epäonnistui")
+
 
 @app.route("/deletemessage/<int:subject_id>/<int:thread_id>/<int:message_id>")
 def delete_message(subject_id, thread_id, message_id):
@@ -64,7 +83,8 @@ def delete_message(subject_id, thread_id, message_id):
         url = f"/subject/{subject_id}/{thread_id}"
         return redirect(url)
     else:
-        return render_template("error.html", message="Viestin poisto epäonnistui")
+        return render_template("messages.html", error_message="Viestin poisto epäonnistui")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
