66ca9f6dcef983ed3b22e2115e1e490b2fec58a3
Matias Nisula <matiasnisula81@gmail.com>
2022-03-02 15:53:55 +0200

    db functionality get_message(id), update() messages.py

--- messages.py
+++ messages.py
@@ -8,6 +8,11 @@ def get_list(thread_id):
     result = db.session.execute(sql, {"id":thread_id})
     return result.fetchall()
 
+def get_message_content(id):
+    sql = "SELECT content FROM messages WHERE id=:id AND visible=TRUE"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchone()
+
 
 def save(content, thread_id):
     user_id = users.user_id()
@@ -26,5 +31,15 @@ def delete(id):
         return False
     sql = "UPDATE messages SET visible=FALSE WHERE id=:id AND user_id=:user_id"
     db.session.execute(sql, {"id":id, "user_id":user_id})
+    db.session.commit()
+    return True
+
+def update(id, thread_id, content):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    sql = "UPDATE messages SET content=:content WHERE id=:id AND user_id=:user_id " \
+        "AND thread_id=:thread_id AND visible=TRUE"
+    db.session.execute(sql, {"content":content, "id":id, "user_id":user_id, "thread_id":thread_id})
     db.session.commit()
     return True
